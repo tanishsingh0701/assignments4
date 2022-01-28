557206b7013eeb99956813908d82edeafe30fd14
tanish0701 <tanish.singh@hatch.com>
2022-01-28 15:24:43 +0530

    cg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'assignments4';
+  oddNumber:number[]=[];
+  evenNumber:number[]=[];
+
+  onIntervalFired(firedNumber:number)
+  {
+    if(firedNumber % 2===0)
+    {
+      this.evenNumber.push(firedNumber);
+    }
+    else
+    {
+      this.oddNumber.push(firedNumber);
+    }
+  }
 }
